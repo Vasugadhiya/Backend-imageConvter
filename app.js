086dab44f02436cb606c3b0c131ebce8b9d04c16
CodebyAnandi <anandi.tanthetaa@gmail.com>
2024-07-27 10:25:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const express = require('express');
+const cors = require('cors');
+const conversionRouter = require('./app/router/conversionRouter'); // Import the router
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use('/download', express.static(process.env.STORAGE_PATH || './upload'));
+app.use('/api', conversionRouter); // Use the router with a base path
+
+app.listen(port, () => {
+  console.log('Server listening successfully on port', port);
+});
